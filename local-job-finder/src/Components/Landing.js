5bf0550fc8cf6637c15b08eb5be03285f42259b0
jason-qin2 <qinzechen@hotmail.com>
2020-09-03 16:28:36 -0400

    Landing Page Stats

--- local-job-finder/src/Components/Landing.js
+++ local-job-finder/src/Components/Landing.js
@@ -1,23 +1,43 @@
-import React, {useState} from 'react';
-import {Jumbotron, Row, Col, Button, Card, CardHeader, CardFooter, CardBody, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Table} from 'reactstrap';
+import React, { useState } from 'react';
+import { Jumbotron, Row, Col, Button, Card, CardHeader, CardFooter, CardBody, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Table } from 'reactstrap';
 
 function Landing() {
     const [collapsed, setCollapsed] = useState(true);
     const toggleNavbar = () => setCollapsed(!collapsed);
 
     return (
-        <Col>
-        <Jumbotron>
-        <h1 className="display-3">Welcome to the fix for all your personal jobs!</h1>
-        <p className="lead">Have you ever wanted your lawn mowed, or your driveway to be plowed but you don't want to do it your self? Well here's the solution. Instead of hiring expensive companies to do the work for you, why not support your local communities and have them do it for you.</p>
-        <hr className="my-2" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p className="lead">
-          <Button color="primary" href="/About/">Learn More</Button>
-        </p>
-      </Jumbotron>
-        </Col>
-        
+        <div>
+
+            <Jumbotron>
+                <h1 className="display-3">Welcome to the solution to all the jobs you need completed!</h1>
+                <p className="lead">Have you ever wanted your lawn mowed, or your driveway to be plowed but you don't want to do it yourself? Well here's the solution: Instead of hiring expensive companies to do the work for you, why not support your local communities and have someone qualified do it for you.</p>
+                <hr className="my-2" />
+                <p>We are excited to bring you the right people for the job.</p>
+                <p className="lead">
+                    <Button color="primary" href="/About/">Learn More</Button>
+                </p>
+            </Jumbotron>
+
+            <Col>
+                <Card>
+                    <CardHeader>
+                        Current Stats
+                </CardHeader>
+                    <CardBody>
+                        <Table>
+                            <tbody>
+                                <Col>
+                                    <tr><th scope='row'>Current Jobs being Offered</th><td>2</td></tr>
+                                    <tr><th scope='row'>Successful Jobs Completed</th><td>2</td></tr>
+                                    <tr><th scope='row'>Total Job-seekers</th><td>2</td></tr>
+                                </Col>
+                            </tbody>
+                        </Table>
+                    </CardBody>
+                </Card>
+            </Col>
+
+        </div>
     );
 }
 
