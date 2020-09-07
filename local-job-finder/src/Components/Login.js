34d58763e94f0da123eca0914064e721fc935327
jason-qin2 <qinzechen@hotmail.com>
2020-09-06 23:46:16 -0400

    login changes

--- local-job-finder/src/Components/Login.js
+++ local-job-finder/src/Components/Login.js
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
 import { Jumbotron, Row, Col, Button, Card, CardHeader, CardFooter, CardBody, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Table, Input } from 'reactstrap';
 
-function Login() {
+class Login extends React.Component {
 
-    return (
-        <div>
-            <Col>
-                <Card style={{ width: "50%", marginLeft: "25%", marginTop:"3%" }}>
-                    <CardHeader>
-                        Login
-                </CardHeader>
-                    <CardBody>
-                        <Table>
-                            <tbody>
-                                <Col>
-                                    <tr><th scope='row'>Username</th><td><Input /></td></tr>
-                                    <tr><th scope='row'>Password</th><td><Input /></td></tr>
-                                </Col>
-                            </tbody>
-                        </Table>
-                    </CardBody>
-                </Card>
-            </Col>
-
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <Col>
+                    <Card style={{ width: "50%", marginLeft: "25%", marginTop:"3%" }}>
+                        <CardHeader>
+                            Login
+                    </CardHeader>
+                        <CardBody>
+                            <Table>
+                                <tbody>
+                                    <Col>
+                                        <tr><th scope='row'>Username</th><td><Input /></td></tr>
+                                        <tr><th scope='row'>Password</th><td><Input /></td></tr>
+                                    </Col>
+                                </tbody>
+                            </Table>
+                        </CardBody>
+                    </Card>
+                </Col>
+    
+            </div>
+        );
+    }
+    
 }
 
 export default Login;
