2ab94c06310fb790c037cce8648e389b12447e40
jason-qin2 <qinzechen@hotmail.com>
2020-09-14 20:46:11 -0400

    job view and user view

--- local-job-finder/src/Components/Jobs.js
+++ local-job-finder/src/Components/Jobs.js
@@ -24,10 +24,10 @@ function Landing() {
                                     <th>Distance</th>
                                 </thead>
                                 <tbody>
-                                    <tr><td scope='row'>Lawn Mowing</td><td><Link to="/Userview/1">Joe Smith</Link></td><td>Mississauga</td><td>9AM to 5PM</td><td>2 km</td></tr>
-                                    <tr><td scope='row'>Driveway Shovel</td><td>Bob Lee</td><td>Mississauga</td><td>9AM to 5PM</td><td>1.2 km</td></tr>
-                                    <tr><td scope='row'>Pool Maintenance</td><td>Dan Jack</td><td>Toronto</td><td>9AM to 5PM</td><td>19 km</td></tr>
-                                    <tr><td scope='row'>Front Lawn Cleaning</td><td>Stella King</td><td>Brampton</td><td>9AM to 5PM</td><td>6 km</td></tr>
+                                    <tr><td scope='row'><Link to="JobView/1">Lawn Mowing</Link></td><td><Link to="/Userview/1">Joe Smith</Link></td><td>Mississauga</td><td>9AM to 5PM</td><td>2 km</td></tr>
+                                    <tr><td scope='row'><Link to="JobView/2">Driveway Shoveling</Link></td><td><Link to="/Userview/2">Bob Lee</Link></td><td>Mississauga</td><td>9AM to 5PM</td><td>1.2 km</td></tr>
+                                    <tr><td scope='row'><Link to="JobView/3">Pool Maintenance</Link></td><td><Link to="/Userview/3">Dan Jack</Link></td><td>Toronto</td><td>9AM to 5PM</td><td>19 km</td></tr>
+                                    <tr><td scope='row'><Link to="JobView/4">Lawn Cleaning</Link></td><td><Link to="/Userview/4">Stella Yang</Link></td><td>Brampton</td><td>9AM to 5PM</td><td>6 km</td></tr>
 
                             </tbody>
                         </Table>
